23b848655f77f9032f572471edb7a7946a66bbb8
Ezra W. <78620375+ewingard@users.noreply.github.com>
2024-07-23 02:17:22 +0000

    created preliminary art camp gallery within projects - to be launched later.

---
+++ projects/art-camp/script.js
@@ -0,0 +1,28 @@
+//Nav Toggle
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+document.getElementById('menuToggle').addEventListener('click', function myFunction(x) {
+    const textMenu = document.getElementById('textMenu');
+    const isExpanded = this.getAttribute('aria-expanded') === 'true';
+    
+    if (textMenu.classList.contains('menu-hidden')) {
+        textMenu.classList.replace('menu-hidden', 'menu-visible');
+      } else {
+        textMenu.classList.replace('menu-visible', 'menu-hidden');
+      }
+
+    x.setAttribute('aria-expanded', !isExpanded);
+
+});
+
+//Mobile Nav Toggle
+function mobileNavToggle() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
