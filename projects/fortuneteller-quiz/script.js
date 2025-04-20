1ee29b3b3a5eea2633091ab91df620f5b6e1a322
Ezra W. <78620375+ewingard@users.noreply.github.com>
2025-04-20 17:27:04 +0000

    fixed fortuneteller mobilenav, modified aboutme page for correct mobile nav.

--- projects/fortuneteller-quiz/script.js
+++ projects/fortuneteller-quiz/script.js
@@ -202,3 +202,14 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+// Mobile Nav Toggle
+function mobileNavToggle() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+    }
+//End Mobile Nav
